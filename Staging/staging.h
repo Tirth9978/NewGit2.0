28ace090463fc9c3673e2244c5aa2ea06c41eee1
Tirth9978 <tirthppatel9978@gmail.com>
2025-10-25 23:42:41 +0530

    I added more

--- Staging/staging.h
+++ Staging/staging.h
@@ -4,5 +4,8 @@
 const char * generateId() ;
 void addingStaging() ;
 void movingFilesToStaheFolder(char * filepath , char* id) ;
+char * getDateTime();
+void copy_file(const char *src_path, const char *dest_path);
+
 
 #endif
