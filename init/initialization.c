0a2b409044c8fde98d876542ed9e0cd5d456665e
Tirth9978 <tirthppatel9978@gmail.com>
2025-10-27 09:39:42 +0530

    I added more

--- init/initialization.c
+++ init/initialization.c
@@ -5,6 +5,7 @@
 #include <errno.h>    // For errno
 #include <unistd.h>
 #include<stdlib.h>
+#include <stdbool.h>  
 #include "initialization.h"
 #include "../Errors/errors.h"
 
@@ -22,6 +23,48 @@ char * gettingPwd() {
      return cwd;  
 }
 
+bool checkInMainFolder(char * pwd) {
+     if (pwd == NULL) {
+          return 0;
+     }
+
+     const char * home = getenv("HOME");
+     if (!home) {
+          forkCreationProblem();
+          return ;
+     }
+
+     char file_path[1024];
+     snprintf(file_path, sizeof(file_path), "%s/NewGit2.0/InitInfo.txt", home);
+
+     FILE * file = fopen(file_path);
+
+     if (!file){
+          forkCreationProblem();
+          return 0;
+     }
+
+     char line[1024];
+     bool found = false;
+
+     while (fgets(line, sizeof(line), file)) {
+          // Remove newline character at the end
+          size_t len = strlen(line);
+          if (len > 0 && line[len - 1] == '\n') {
+               line[len - 1] = '\0';
+          }
+
+          // Compare with the PWD we're checking
+          if (strcmp(line, pwd) == 0) {
+               found = true;
+               break;
+          }
+     }
+
+     fclose(file); // Closing the file is the important .... :)
+     return found;
+}
+
 void makingDotGitFolder(){
      const char * folderName = ".newgit";
      if (mkdir(folderName , 0755) == 0) {
@@ -36,7 +79,11 @@ void makingDotGitFolder(){
 
 void makingStagingIdInfoFile() {
      char *current_dir = gettingPwd();
-
+     if (checkInMainFolder(current_dir)) {
+          printf(GRN "This repo already Initialized :)\n" END);
+          printf(CYN "NewGit2.0 ---> 1.0.1\n" END);
+          return ;
+     }
      if (current_dir == NULL) {
           ProblemInInit();
           return ; 
