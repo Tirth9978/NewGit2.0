982ed22ea4baf1a4448d5ba03b9effcc32b8abe0
Tirth9978 <tirthppatel9978@gmail.com>
2025-10-30 14:14:48 +0530

    Just Adde the small srand()

--- Staging/staging.c
+++ Staging/staging.c
@@ -23,33 +23,39 @@
 #define VERSION "1.0.1"
 
 const char * generateId() {
-     const char * data1 = "ASDFGHJKLZXCVBNMPOIUYTREWQ";
-     const char * data2 = "1234567890";
-     const char * data3 = "qwertyuioplkjhgfdsazxcvbnm";
-     const char * data4 = "!@#$&*";
+    const char * data1 = "ASDFGHJKLZXCVBNMPOIUYTREWQ";
+    const char * data2 = "1234567890";
+    const char * data3 = "qwertyuioplkjhgfdsazxcvbnm";
+    const char * data4 = "!@#$&*";
 
-     static char ans[100];
-     ans[0] = '\0';  // clear buffer
+    static char ans[100];
+    ans[0] = '\0';
 
-     for (int count = 0; count < 3; count++) {
-          char temp[2];
-          temp[1] = '\0';
+    // Seed rand only once
+    static int seeded = 0;
+    if (!seeded) {
+        srand(time(NULL));
+        seeded = 1;
+    }
 
-          temp[0] = data1[rand() % 26]; 
-          strcat(ans, temp);
+    for (int count = 0; count < 3; count++) {
+        char temp[2];
+        temp[1] = '\0';
 
-          temp[0] = data2[rand() % 10]; 
-          strcat(ans, temp);
+        temp[0] = data1[rand() % 26];
+        strcat(ans, temp);
 
-          temp[0] = data3[rand() % 26]; 
-          strcat(ans, temp);
+        temp[0] = data2[rand() % 10];
+        strcat(ans, temp);
 
-          temp[0] = data4[rand() % 6];  
-          strcat(ans, temp);
-          
-     }
+        temp[0] = data3[rand() % 26];
+        strcat(ans, temp);
 
-     return ans;
+        temp[0] = data4[rand() % 6];
+        strcat(ans, temp);
+    }
+
+    return ans;
 }
 
 char* getDateTime() {
