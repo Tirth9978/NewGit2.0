686c8f7617d53f3641b9f58f857652a63028bccd
Tirth9978 <tirthppatel9978@gmail.com>
2025-11-07 09:20:21 +0530

    I add the StagFile Count

--- Commit/commit.c
+++ Commit/commit.c
@@ -64,6 +64,45 @@ struct FileCount countCurrentDir() {
      return count;
 }
 
+struct FileCount countInStagDIR(char * id){
+     struct FileCount count= {-1,-1};
+     char path[200];
+     snprintf(path, sizeof(path), ".newgit/StagingInfo/%s", id);
+     struct dirent *dp;
+     struct stat st;
+     DIR *dir = opendir(path);
+
+
+     if (!dir)
+        return count; 
+     
+     count.files = 0 ;
+     count.folders = 0;
+
+
+     while ((dp = readdir(dir)) != NULL) {
+          // Ignore . and ..
+          if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
+               continue;
+
+          char path[1024];
+          snprintf(path, sizeof(path), "%s/%s", path, dp->d_name);
+
+          if (stat(path, &st) == 0) {
+               if (S_ISDIR(st.st_mode)) {
+                    count.folders++;
+                    struct FileCount subCount = countInStagDIR(path);
+                    count.files += subCount.files;
+                    count.folders += subCount.folders;
+               } else if (S_ISREG(st.st_mode)) {
+                    count.files++;
+               }
+          }
+     }
+     closedir(dir);
+     return count ;
+}
+
 
 char *getingId() {
      const char *path = ".newgit/idInfo.txt";
@@ -128,6 +167,8 @@ void appendMessage(char * message) {
      }
      // Counting the Numbers-----------------------------------
      struct FileCount currCount =countCurrentDir(); 
+     struct FileCount stagCount = countInStagDIR(id);
+     printf("%d %d" , stagCount.files , stagCount.folders);
      if (currCount.files == -1 && currCount.folders == -1) {
           problemInCommit();
           return ;
