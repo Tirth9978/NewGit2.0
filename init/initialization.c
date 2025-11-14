c39af6ff844844795d9a396dd87abb2ea4050fde
Tirth9978 <tirthppatel9978@gmail.com>
2025-11-14 09:28:55 +0530

    Just added the sutup.out file

--- init/initialization.c
+++ init/initialization.c
@@ -51,7 +51,6 @@ bool checkInMainFolder(char * pwd) {
      FILE * file = fopen(file_path , "r");
   
      if (file==NULL){
-          printf("I am Here\n");
           forkCreationProblem();
           return 0;
      }
